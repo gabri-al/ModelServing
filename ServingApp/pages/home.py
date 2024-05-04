97895ce6d7c8de1b8eb9495a2ea446e8f78b40d9
Gabriele Albini <gabri.albini@gmail.com>
2024-05-04 18:28:50 +0200

    API interaction added

--- ServingApp/pages/home.py
+++ ServingApp/pages/home.py
@@ -1,6 +1,7 @@
 import dash
-from dash import html, dcc, callback, Input, Output, State
+from dash import html, dcc, callback, Input, Output
 import dash_bootstrap_components as dbc
+import pandas as pd
 
 dash.register_page(__name__, path='/', name='Home', title='Model Serving')
 
@@ -22,6 +23,10 @@ def generate_feature_layout(_fields):
     ], className = 'IO-inner-row-wButton')
     _div.append(button_row)
     return html.Div(_div, className = "IO-outer-col")
+
+############################################################################################
+# Function to perform inference
+from assets.query_endpoint import score_model
 
 ############################################################################################
 # Page layout
@@ -49,13 +54,67 @@ layout = dbc.Container([
             generate_feature_layout(_fields)
         ], width = 5),
         dbc.Col([
+            html.Div([
+                dbc.Row([
+                    dbc.Col([], width = 1),
+                    dbc.Col([], id="inference-col", width = 10),
+                    dbc.Col([], width = 1)
+                ], className = 'IO-inner-row')
+            ], className = "IO-outer-col")
         ], width = 5),
         dbc.Col([], width = 1)
     ]),
-
-
     
 ])
 
 ############################################################################################
+# Inference content
+@callback(
+    Output(component_id='inference-col', component_property='children'),
+    Input(component_id='feature0', component_property='value'),
+    Input(component_id='feature1', component_property='value'),
+    Input(component_id='feature2', component_property='value'),
+    Input(component_id='feature3', component_property='value'),
+    Input(component_id='feature4', component_property='value'),
+    Input(component_id='feature5', component_property='value'),
+    Input(component_id='feature6', component_property='value'),
+    Input(component_id='feature7', component_property='value'),
+    Input(component_id='feature8', component_property='value'),
+    Input(component_id='feature9', component_property='value'))
+def data_transform(feature0, feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9):
+    _output = []
+    try:
+        feature0clean = float(feature0)
+        feature1clean = float(feature1)
+        feature2clean = float(feature2)
+        feature3clean = float(feature3)
+        feature4clean = float(feature4)
+        feature5clean = float(feature5)
+        feature6clean = float(feature6)
+        feature7clean = float(feature7)
+        feature8clean = float(feature8)
+        feature9clean = float(feature9)
+        feature_df = pd.DataFrame({
+            'latitude': [feature0clean],
+            'longitude': [feature1clean],
+            'is_entire_apt': [feature2clean],
+            'is_private_room': [feature3clean],
+            'is_shared_room': [feature4clean],
+            'minimum_nights': [feature5clean],
+            'number_of_reviews': [feature6clean],
+            'reviews_per_month': [feature7clean],
+            'calculated_host_listings_count': [feature8clean],
+            'availability_365': [feature9clean]})
+    except:
+        _output = dbc.Alert(children=['Insert valid features'], color='warning', class_name='alert-style')
+        return _output
 
+    _response = score_model(feature_df)
+    print(_response)
+    if _response.status_code != 200: # Failure
+        _output = dbc.Alert(children=['Request failed with status: '+str(_response.status_code)+', '+str(_response.text)],
+                            color='danger', class_name='alert-style-danger')
+    else:
+        _output = dbc.Alert(children=['Success'],color='success', class_name='alert-style')
+    
+    return _output
