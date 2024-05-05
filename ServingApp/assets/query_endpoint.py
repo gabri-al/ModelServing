3b5ea3404e43c49e9e3c60529104eeaed68fa545
Gabriele Albini <gabri.albini@gmail.com>
2024-05-05 08:53:52 +0200

    API interaction completed

--- ServingApp/assets/query_endpoint.py
+++ ServingApp/assets/query_endpoint.py
@@ -2,10 +2,8 @@ import requests
 import json
 import pandas as pd
 
-"""
 def create_tf_serving_json(data):
   return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
-"""
 
 def score_model(dataset):
     url = 'https://e2-demo-field-eng.cloud.databricks.com/serving-endpoints/g_testab_endpoint/invocations'
@@ -13,8 +11,7 @@ def score_model(dataset):
     headers = {
         'Authorization': f'Bearer {TOKEN}',
         'Content-Type': 'application/json'}
-    #data_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
-    data_dict = {'dataframe_split': dataset.to_dict(orient='split')}
+    data_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
     data_json = json.dumps(data_dict, allow_nan=True)
     print(data_json)
     response = requests.request(method='POST', headers=headers, url=url, data=data_json)
