b68a4a864cd53fdd45dacbffcde407cf86f0cbcf
gabri-al <gabri-al>
2024-10-25 08:44:08 +0000

    Footer msg improved

--- 03_WebApp_setup/app_/assets/footer.py
+++ 03_WebApp_setup/app_/assets/footer.py
@@ -5,8 +5,11 @@ _footer = html.Div([
     dbc.Container([
         dbc.Row([
             dbc.Col([], width = 1),
-            dbc.Col(['Showcasing Databricks Model Serving via Plotly Dash'], width = 6),
-            dbc.Col([], width = 1),
+            dbc.Col([
+                html.Img(src="assets/img/DatabricksLogo.png", alt="image", disable_n_clicks = True, className = 'databricks-img'),
+                html.Img(src="assets/img/PlotlyDash.png", alt="image", disable_n_clicks = True, className = 'plotly-img'),
+                'Web-App developed, built, hosted on Databricks'
+            ], className = 'footer-img-col', width = 7),
 	        dbc.Col([
                 html.Ul([
                     html.Li([
